16cfce7c714315c20ba89c173f30fee8bf662dd7
Rotimiiam <rotimiiam@gmail.com>
2023-06-29 11:25:23 +0100

    complete

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amp2484d9ce": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3content323": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
